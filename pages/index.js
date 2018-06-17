2e270a615e77bf0cbe917df1619b6d96e2865e6e
Chase Starr <chasestarr@gmail.com>
2018-06-17 15:04:48 -0700

    pulling data from the cms

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import { createClient } from '../lib/contentful';
+
 class Index extends React.Component {
+  static async getInitialProps() {
+    const client = createClient();
+    return client
+      .getEntries({
+        content_type: 'landmark',
+        order: '-sys.createdAt',
+      })
+      .then(response => ({
+        landmarks: response.items,
+      }));
+  }
+
   render() {
+    console.log(this.props.landmarks);
     return <div>hello</div>;
   }
 }
