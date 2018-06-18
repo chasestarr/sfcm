d0400f471c8d7cc97e4bcd919c547368e81bb91d
Chase Starr <chasestarr@gmail.com>
2018-06-18 08:10:52 -0700

    static setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 const withCSS = require('@zeit/next-css');
+const contentful = require('contentful');
+
+const client = contentful.createClient({
+  space: process.env.SPACE_ID,
+  accessToken: process.env.ACCESS_TOKEN,
+});
+
 module.exports = withCSS({
   cssLoaderOptions: {
     importLoaders: 1,
+  },
+  exportPathMap: async function(defaultPathMap) {
+    const data = await client.getEntries({
+      content_type: 'landmark',
+      order: '-sys.createdAt',
+    });
+
+    const landmarks = data.items.map(item => item.fields.slug).reduce((acc, cur) => {
+      acc[`/${cur}`] = { page: '/landmark', query: { slug: cur } };
+      return acc;
+    }, {});
+
+    return {
+      '/': { page: '/' },
+      ...landmarks,
+    };
   },
 });
